1880455ea3ee66c664edb6d003d7ea100fc8c249
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-07-08 16:35:39 +0200

    Add manifest and Apache module
    Add Apache module (as a Git submodule) from Example42
    Add puppi module (needed by Apache)
    Add config file for apache vhost.
    Add Manifest.

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,30 @@
+# Basic Puppet manifest
+
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+class system-update {
+
+  exec { 'apt-get update':
+    command => 'apt-get update',
+  }
+
+  $sysPackages = [ "build-essential" ]
+  package { $sysPackages:
+    ensure => "installed",
+    require => Exec['apt-get update'],
+  }
+}
+
+class apache-setup {
+
+    include apache
+
+    apache::module { 'proxy': }
+    apache::module { 'proxy_fcgi': }
+}
+
+Exec["apt-get update"] -> Package <| |>
+
+include system-update
+
+include apache-setup
