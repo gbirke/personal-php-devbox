be934ac969046a0c47da0b80afcbabbcd921315d
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-07-09 17:47:51 +0200

    Add Elasticsearch module
    Add Java and Apt modules (needed by elasticsearch)
    Configure elasticsearch for mediawiki
    Replace double quotes with singel quotes.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,6 +1,6 @@
 # Basic Puppet manifest
 
-Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 
 class system-update {
 
@@ -8,23 +8,23 @@ class system-update {
     command => 'apt-get update',
   }
 
-  $sysPackages = [ "build-essential" ]
+  $sysPackages = [ 'build-essential' ]
   package { $sysPackages:
-    ensure => "installed",
+    ensure => 'installed',
     require => Exec['apt-get update'],
   }
 }
 
 class development-essentials {
 
-  $devPackages = [ "git", "vim", "curl" ]
+  $devPackages = [ 'git', 'vim', 'curl' ]
   package { $devPackages:
-    ensure => "installed"
+    ensure => 'installed'
   }
 
   # TODO https://forge.puppetlabs.com/acme/ohmyzsh
   # TODO download git and vim config
-  # TODO install ack-grep package and alias to "ack command"
+  # TODO install ack-grep package and alias to 'ack command'
   # TODO https://code.google.com/p/psgrep/
 
 }
@@ -103,8 +103,8 @@ class roles::php_fpm($version = 'installed') {
 
   Php::Extension <| |> ~> Service['php5-fpm']
 
-  exec { "restart-php5-fpm":
-    command  => "service php5-fpm restart",
+  exec { 'restart-php5-fpm':
+    command  => 'service php5-fpm restart',
     schedule => hourly
   }
 
@@ -112,16 +112,11 @@ class roles::php_fpm($version = 'installed') {
 
 }
 
-class { 'php::phpunit':
-  ensure => latest
-}
-
-
 class sites {
 
-  $global_db_user = "webuser"
-  $global_db_pass = "password123"
-  $global_db_grant = ["ALL"]
+  $global_db_user = 'webuser'
+  $global_db_pass = 'password123'
+  $global_db_grant = ['ALL']
   apache::vhost { 'spenden.wikimedia.dev':
    docroot  => '/vagrant/www/spenden',
    template => '/vagrant/conf/apache/vhost.conf.erb',
@@ -137,10 +132,102 @@ class sites {
     host => 'localhost',
     grant => $global_db_grant
   }
+  elasticsearch::instance { 'es-01':
+    config => {
+      'node.name' => 'elasticsearch01',
+      'script.disable_dynamic' => false,
+      'network.host' => '127.0.0.1',
+      'script.groovy.sandbox.class_whitelist' => [
+          # Defaults
+          'java.util.Date',
+          'java.util.Map',
+          'java.util.List',
+          'java.util.Set',
+          'java.util.ArrayList',
+          'java.util.Arrays',
+          'java.util.HashMap',
+          'java.util.HashSet',
+          'java.util.UUID',
+          'java.math.BigDecimal',
+          'org.joda.time.DateTime',
+          'org.joda.time.DateTimeZone',
+          'org.elasticsearch.common.joda.time.DateTime',
+          'org.elasticsearch.common.joda.time.DateTimeZone',
+          # Added for Cirrus
+          'java.util.Locale',
+          'org.apache.lucene.util.automaton.RegExp',
+          'org.apache.lucene.util.automaton.CharacterRunAutomaton'
+      ],
+      'script.groovy.sandbox.package_whitelist' => [
+          # Defaults
+          'java.util',
+          'java.lang',
+          'org.joda.time',
+          'org.elasticsearch.common.joda.time',
+          # Added for Cirrus
+          'org.apache.lucene.util.automaton'
+      ],
+      'script.groovy.sandbox.receiver_whitelist' => [
+          # Defaults
+          'java.lang.Math',
+          'java.lang.Integer',
+          '"[I"',
+          '"[[I"',
+          '"[[[I"',
+          'java.lang.Float',
+          '"[F"',
+          '"[[F"',
+          '"[[[F"',
+          'java.lang.Double',
+          '"[D"',
+          '"[[D"',
+          '"[[[D"',
+          'java.lang.Long',
+          '"[J"',
+          '"[[J"',
+          '"[[[J"',
+          'java.lang.Short',
+          '"[S"',
+          '"[[S"',
+          '"[[[S"',
+          'java.lang.Character',
+          '"[C"',
+          '"[[C"',
+          '"[[[C"',
+          'java.lang.Byte',
+          '"[B"',
+          '"[[B"',
+          '"[[[B"',
+          'java.lang.Boolean',
+          '"[Z"',
+          '"[[Z"',
+          '"[[[Z"',
+          'java.math.BigDecimal',
+          'java.math.BigDecimal',
+          'java.util.Arrays',
+          'java.util.Date',
+          'java.util.List',
+          'java.util.Map',
+          'java.util.Set',
+          'java.lang.Object',
+          'org.joda.time.DateTime',
+          'org.joda.time.DateTimeUtils',
+          'org.joda.time.DateTimeZone',
+          'org.joda.time.Instant',
+          'org.elasticsearch.common.joda.time.DateTime',
+          'org.elasticsearch.common.joda.time.DateTimeUtils',
+          'org.elasticsearch.common.joda.time.DateTimeZone',
+          'org.elasticsearch.common.joda.time.Instant',
+          # Added for Cirrus
+          'org.apache.lucene.util.automaton.RegExp',
+          'org.apache.lucene.util.automaton.CharacterRunAutomaton'
+      ]
+    }
+  }
 
 }
 
-Exec["apt-get update"] -> Package <| |>
+Exec['apt-get update'] -> Package <| |>
 
 include system-update
 
@@ -152,5 +239,18 @@ include 'roles::php_fpm'
 #TODO install other PHP QA and documentation tools
 
 include development-essentials
+
+class { 'php::phpunit':
+  ensure => latest
+}
+
+# Install Elasticsearch
+class { 'elasticsearch':
+  autoupgrade => true,
+  manage_repo  => true,
+  repo_version => '1.4',
+  java_install => true
+}
+
 
 include sites
