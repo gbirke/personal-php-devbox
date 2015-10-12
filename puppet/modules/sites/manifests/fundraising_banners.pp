893ab0760933afeca9bdcb02a7b6e8918aed592d
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-10-12 16:56:34 +0200

    Add alias to banner host

--- puppet/modules/sites/manifests/fundraising_banners.pp
+++ puppet/modules/sites/manifests/fundraising_banners.pp
@@ -3,6 +3,7 @@ class sites::fundraising_banners {
   include sites::params
 
   apache::vhost { 'banners.wikimedia.dev':
+   serveraliases => 'banner.wikimedia.dev',
    docroot  => '/vagrant/www/FundraisingBanners',
    template => 'sites/vhost_php.conf.erb',
  }
